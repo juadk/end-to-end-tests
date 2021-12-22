a683572490c7ac645679c75b0bf527cbe8a408a1
Julien ADAMEK <julien.adamek@suse.com>
2021-12-22 23:46:42 +0100

    Project init

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+require('dotenv').config();
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  const url = process.env.DEV_UI || 'https://localhost:8005';
+
+  config.baseUrl = url.replace(/\/$/, '');
+
+  config.env.username = process.env.TEST_USERNAME;
+  config.env.password = process.env.TEST_PASSWORD;
+  config.env.cluster = process.env.TEST_CLUSTER;
+  config.env.system_domain = process.env.TEST_SYSTEM_DOMAIN
+
+  return config;
+};
